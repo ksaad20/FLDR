22e94ebe9a12e37d286dcf8802bcbb4077fe9876
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-07-27 01:43:14 +0600

    Update __init__.py to include Pipeline in __all__

--- fldr/__init__.py
+++ fldr/__init__.py
@@ -1,8 +1,15 @@
-from .pipeline import Pipeline 
+"""FLDR - Fault Line Detection and Reporting Framework."""
+
+from .pipeline import Pipeline
 from .report import InspectionReport
 
+__version__ = "0.0.1"
 __author__ = "Asif Kazi"
 __email__ = "kazisaadasif29@gmail.com"
 __license__ = "Apache-2.0"
 
-__all__ = ["__version__", "run_inspection", "InspectionReport"]
+__all__ = [
+    "Pipeline",
+    "InspectionReport",
+    "__version__",
+]
